03f9774c166a9a1ee7c7858ca6a6cb7477686c74
Dmitry Prudnikov <mail@polaz.com>
2026-01-23 22:37:56 +0200

    feat(docs): improve widget visibility, add logo, widen layout
    - Widget: move from vertical side tab to floating bottom-right button
      with pulse animation and "Report Bug" label (more discoverable)
    - Logo: add project logo to navbar and hero section on landing page
    - Layout: add custom CSS with @media queries for wider content on
      screens >1440px and >1600px (reduces wasted horizontal space)

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,7 @@
 import { h } from "vue";
 import DefaultTheme from "vitepress/theme";
 import type { Theme } from "vitepress";
+import "./style.css";
 import BugReportWidget from "./components/BugReportWidget.vue";
 
 export default {

675ea12cb1104250361f8131934eb5b98bb9aa9e
Dmitry Prudnikov <mail@polaz.com>
2026-01-25 17:32:16 +0200

    feat(docs): add Google Analytics tracking to VitePress documentation (#216)
    - Integrate vitepress-plugin-google-analytics for clean SPA navigation handling
    - Support VITE_GA_ID env var override, default to G-RY1XJ7LR5F
    
    Closes #214

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,7 @@
 import { h } from "vue";
 import DefaultTheme from "vitepress/theme";
 import type { Theme } from "vitepress";
+import googleAnalytics from "vitepress-plugin-google-analytics";
 import "./style.css";
 import BugReportWidget from "./components/BugReportWidget.vue";
 
@@ -9,6 +10,11 @@ export default {
   Layout() {
     return h(DefaultTheme.Layout, null, {
       "layout-bottom": () => h(BugReportWidget),
+    });
+  },
+  enhanceApp({ app }) {
+    googleAnalytics({
+      id: import.meta.env.VITE_GA_ID || "G-RY1XJ7LR5F",
     });
   },
 } satisfies Theme;
